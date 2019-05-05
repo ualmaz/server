d6b5db71e32bf9a845d08295f0be4c79a20f8b3b
kazak <kazak@ualmaz@gmail.com>
2019-05-05 22:02:01 +0600

    req.txt

--- users/models.py
+++ users/models.py
@@ -17,7 +17,7 @@ class User(AbstractUser):
     country = models.CharField(verbose_name="Country name", max_length=255)
     city = models.CharField(verbose_name="City name", max_length=255)
     email = models.EmailField(verbose_name="Email", max_length=255)
-    is_active = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
 
     def __str__(self):
         return '{} {}' .format(self.first_name, self.last_name)
