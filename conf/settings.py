6d5fda86608ed812fcf85923a28ffce92eccd7d6
kazak <kazak@ualmaz@gmail.com>
2019-05-05 20:48:07 +0600

    req.txt

--- conf/settings.py
+++ conf/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '7a2(#rv8umi0802)5r7ox00!l4_f_d2s*^t!r=3a8w4ffd28$b'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['emeupci.com', '178.62.50.126', 'localhost', '127.0.0.1']
 
 
 # Application definition
---
+++ conf/settings_prod.py
@@ -0,0 +1,13 @@
+from .settings import *
+
+
+DATABASES = {
+   'default': {
+       'ENGINE': 'django.db.backends.postgresql_psycopg2',
+       'NAME': 'locator',
+       'USER': 'scott',
+       'PASSWORD': 'Acts2:38-39',
+       'HOST': 'localhost',
+       'PORT': '',
+   }
+   }
