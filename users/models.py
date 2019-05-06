ba5c275bb71c575d2e45fbad3a04b4420edccf7f
kazak <kazak@ualmaz@gmail.com>
2019-05-06 13:07:20 +0600

    a

--- users/models.py
+++ users/models.py
@@ -10,14 +10,14 @@ from django.core.mail import send_mail
 from django.utils.dates import MONTHS
 from datetime import date
 
-
+True
 class User(AbstractUser):
     first_name = models.CharField(verbose_name="First name", max_length=255)
     last_name = models.CharField(verbose_name="Last name", max_length=255)
     country = models.CharField(verbose_name="Country name", max_length=255)
     city = models.CharField(verbose_name="City name", max_length=255)
     email = models.EmailField(verbose_name="Email", max_length=255)
-    is_active = models.BooleanField(default=True)
+    is_active = models.BooleanField(default=False)
 
     def __str__(self):
         return '{} {}' .format(self.first_name, self.last_name)
