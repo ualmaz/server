afcb84a23451d665c11db3bef7f509e276cb38f8
kazak <kazak@ualmaz@gmail.com>
2019-07-14 16:27:13 +0600

    fields

--- users/admin.py
+++ users/admin.py
@@ -2,12 +2,19 @@ from django.contrib import admin
 from .models import Area, Country, Post, User, Profile, Report
 
 
-admin.site.register(User)
+
 admin.site.register(Area)
 admin.site.register(Country)
 admin.site.register(Post)
 admin.site.register(Profile)
 admin.site.register(Report)
+
+class CustomUserAdmin(admin.ModelAdmin):
+    list_display = ('username','first_name', 'last_name', 'is_active')
+
+
+
+admin.site.register(User, CustomUserAdmin)
 
 admin.site.site_header = 'Welcome World Changer!'
 
