2f27e4cabd0d14d18b5c2d760e272fbf66852251
ualmaz <ualmaz@gmail.com>
2020-05-13 14:32:19 +0600

    ddd

--- users/context_processors.py
+++ users/context_processors.py
@@ -1,5 +1,11 @@
 from .models import User
 
 def users_id_list(request):
-    user = User
-    return {'users_id_list': User.objects.filter(pk__in=[2, 5])}
+    
+    user = User.objects.filter(pk__in=[2, 5])
+
+    context = {
+        'users_id_list': user )
+    }
+
+    return context
