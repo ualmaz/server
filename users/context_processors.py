1dadd762c3ef9bb64ea1931b459a02b7ed74d869
ualmaz <ualmaz@gmail.com>
2020-05-13 14:33:30 +0600

    ddd

--- users/context_processors.py
+++ users/context_processors.py
@@ -5,7 +5,7 @@ def users_id_list(request):
     user = User.objects.filter(pk__in=[2, 5])
 
     context = {
-        'users_id_list': user )
+        'users_id_list': user
     }
 
     return context
