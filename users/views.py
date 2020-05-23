bfc973b0b270271386a76f9bb3ab9e641a83f6b6
ualmaz <ualmaz@gmail.com>
2020-05-23 23:47:42 +0600

    restore

--- users/views.py
+++ users/views.py
@@ -244,7 +244,9 @@ class CalendarView(ListView):
     form_class = CalendarForm
     template_name = 'calendar/calendar.html'
     context_object_name = 'calendar'
-    ordering = ['-date_posted']
+
+    class Meta:
+        ordering = ['-date_posted']
 
 
 class CalendarDetailView(DetailView):
