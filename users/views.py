e317f51596d18b65784c6ddf3e46a5ba3a0e418f
ualmaz <ualmaz@gmail.com>
2020-05-24 13:24:03 +0600

    api key

--- users/views.py
+++ users/views.py
@@ -244,12 +244,12 @@ class CalendarView(ListView):
     form_class = CalendarForm
     template_name = 'calendar/calendar.html'
     context_object_name = 'calendar'
-    ordering = ['-date_posted']
+    ordering = ['-id']
 
 
 class CalendarDetailView(DetailView):
     model = Calendar
-    ordering = ['-date_posted']
+    ordering = ['-id']
 
 class CalendarCreateView(CreateView):
     model = Calendar
