2b04aa45a30952e92498b457a2342ddd295c97e3
ualmaz <ualmaz@gmail.com>
2020-05-26 16:08:01 +0600

    pagination

--- users/static/users/js/youtube.js
+++ users/static/users/js/youtube.js
@@ -1,4 +1,6 @@
 var reqURL = "https://api.rss2json.com/v1/api.json?rss_url=" + encodeURIComponent("https://www.youtube.com/feeds/videos.xml?channel_id=");
+var channelID = 'UCrhdp1UfyUQG7GYJSF7xUVQ';
+var apiKey = 'AIzaSyDchLsIgc903isuUh9BnVHMyAJE7CwK8i0';
 
 function loadVideo(iframe) {
   $.getJSON(reqURL + iframe.getAttribute('cid'),
@@ -8,6 +10,14 @@ function loadVideo(iframe) {
       var link = data.items[videoNumber].link;
       id = link.substr(link.indexOf("=") + 1);
       iframe.setAttribute("src", "https://youtube.com/embed/" + id + "?controls=0&autoplay=0");
+
+      var url = 'https://www.youtube.com/channel/' + channelID_tuttle;
+      $.getJSON('https://www.googleapis.com/youtube/v3/videos?id=' + id + '&key=' + apiKey_tuttle + '&fields=items(snippet(title))&part=snippet', {format: 'json', url: url}, function (data) {
+        var h2s = $("h4")
+        console.log(data.items)
+        $(h2s[videoNumber]).html(data.items[0].snippet.title)
+
+      });
     }
   );
 }
