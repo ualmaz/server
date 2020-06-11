09d0b7e3f4d24e1d3e97fb4493ef45d5006e8b9d
ualmaz <ualmaz@gmail.com>
2020-06-11 22:52:07 +0600

    static

--- users/urls.py
+++ users/urls.py
@@ -1,4 +1,5 @@
 from django.urls import include, path
+from django.conf.urls.static import static
 from django.conf import settings
 from .views import (
     UserRegistrationView,
@@ -46,4 +47,5 @@ urlpatterns = [
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
